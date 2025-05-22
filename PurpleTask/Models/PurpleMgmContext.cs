d6d129bc8a7c76ddfaab90032bbeee13a31b54db
gustavofourway <68023812+gustavofourway@users.noreply.github.com>
2025-05-22 12:34:22 -0300

    22-05-2025

--- PurpleTask/Models/PurpleMgmContext.cs
+++ PurpleTask/Models/PurpleMgmContext.cs
@@ -45,10 +45,6 @@ public partial class PurpleMgmContext : DbContext
 
     public virtual DbSet<Usr> Usrs { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-       // => optionsBuilder.UseSqlServer("Server=localhost; Database=Purple_mgm; User Id=sa; Password=sa.2014; TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Ac>(entity =>
